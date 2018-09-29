0f19d99994b4c55c588b25af7d2bfee4630acca0
王文祥 <wangwenxiang@youyuwo.com>
2018-09-29 09:50:50 +0800

    init

---
+++ src/main/java/com/plus/schedule/Executor.java
@@ -0,0 +1,16 @@
+package com.plus.schedule;
+
+/**
+ * @author plus me
+ * @date 2018/9/29
+ */
+@FunctionalInterface
+public interface Executor {
+
+    /**
+     * execut process.
+     *
+     * @return a void
+     */
+    void execute();
+}
